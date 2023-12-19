d2c5ac20ef5712b7b2dbc80ac863cebf3df8adff
Samar Chandra <samarc476@gmail.com>
2023-12-19 16:54:50 +0530

    Added new class and unit test

--- Sources/WeatherAppMainViewFramework/WeatherAppMainViewFramework.swift
+++ Sources/WeatherAppMainViewFramework/WeatherAppMainViewFramework.swift
@@ -1,2 +1,26 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct WeatherAppMainViewFramework {
+    public struct MainTextField: View {
+        @State var placeholder: String
+        @Binding var text: String
+        public init(placeholder: String, text: Binding<String>) {
+            // State Value
+            self._placeholder = State(initialValue: placeholder)
+            self._text = text
+        }
+        public var body: some View {
+            HStack{
+                Image(systemName: "person")
+                    .foregroundColor(.blue)
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .foregroundColor(.blue)
+            }
+            .padding()
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
+        }
+    }
+}
--- Tests/WeatherAppMainViewFrameworkTests/WeatherAppMainViewFrameworkTests.swift
+++ Tests/WeatherAppMainViewFrameworkTests/WeatherAppMainViewFrameworkTests.swift
@@ -1,12 +1,22 @@
 import XCTest
+import SwiftUI
 @testable import WeatherAppMainViewFramework
 
 final class WeatherAppMainViewFrameworkTests: XCTestCase {
+    @State private var text: String
+    public init(text: String) {
+        self.text = text
+        super.init()
+    }
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        var body: some View {
+            WeatherAppMainViewFramework.MainTextField(placeholder: "Texting", text: $text)
+        }
+        
     }
 }
